f0e691d79ee6731d1f687311c02a8eeb0300ddc7
hexafraction <hexafraction@gmail.com>
2017-01-03 22:02:46 -0500

    Broken current version

--- vxl-client/src/test/java/me/akhmetov/vxl/client/rendering/GameRenderer.java
+++ vxl-client/src/test/java/me/akhmetov/vxl/client/rendering/GameRenderer.java
@@ -21,6 +21,7 @@ import me.akhmetov.vxl.api.map.BaseMapNode;
 import me.akhmetov.vxl.api.map.MapNode;
 import me.akhmetov.vxl.api.rendering.BlockNodeAppearance;
 import me.akhmetov.vxl.api.rendering.NodeAppearance;
+import me.akhmetov.vxl.api.rendering.RenderBucket;
 import me.akhmetov.vxl.api.rendering.Texture;
 import me.akhmetov.vxl.core.GameState;
 import me.akhmetov.vxl.core.map.HardcodedNodes;
@@ -33,7 +34,7 @@ import org.lwjgl.opengl.GL13;
 public class GameRenderer implements ApplicationListener {
 
     /* TODO next few revisions gameplan:
-     1) Get a working shader of my own being loaded from resources. DONE
+     1) Get a working shaderManager of my own being loaded from resources. DONE
      1a) Wire up the necessary attributes and uniforms including minimal animation. ALMOST DONE (todo some uniforms and attrs for animation)
      2a) Create and test dropping a node. All the data structures are there, just do the add in reverse (drop visible, add hidden faces) DONE
      2) Clean up code massively (by reimplementing algorithms atop new structure). Allow separating by atlas pages, meshes, etc and extend possible attributes.
@@ -46,12 +47,15 @@ public class GameRenderer implements ApplicationListener {
     ActiveArea lam = new NaiveActiveArea();
     MapNode nd1;
     MapNode nd2;
+    MapNode nd2a;
     NodeAppearance ap1;
     NodeAppearance ap2;
+    NodeAppearance ap2a;
     Texture tex1;
     RenderContext rc;
     Texture tex2;
     Texture tex3;
+    Texture tex2a;
     private ModelBatch mb;
     PixmapPacker pp = new PixmapPacker(2048, 2048, Pixmap.Format.RGBA8888, 1, true);
     PackingWrapper wrp = new PackingWrapper(pp);
@@ -59,10 +63,10 @@ public class GameRenderer implements ApplicationListener {
     GameState gs = new GameState(null, null, true);
     NodeResolutionTable nrt = new NodeResolutionTable(gs);
 
-    OldRenderedChunk chk;
+    RenderedChunk chk;
     private NodeTexAtlas atl;
     private ShaderProgram shaderProgram;
-    private RigidBlockNodeShader shader;
+    private ShaderManager shaderManager;
 
 
     public GameRenderer() throws Exception {
@@ -76,14 +80,17 @@ public class GameRenderer implements ApplicationListener {
     float pX, pY, pZ;
     Array<Renderable> renderables = new Array<>(48);
     Pool<Renderable> renderablePool = new ReflectionPool<Renderable>(Renderable.class);
+
     @Override
     public void create() {
         logger.warn("This is a temporary, problematic rendered. TODOs: Dropping nodes, transparency (both representing in appearance and " +
                 "actually rendering, tons of testing, multiple atlases, general cleanup");
         environment = new Environment();
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        shaderManager = new ShaderManager();
         cam.position.set(2f, 2f, 2f);
-        cam.lookAt(0,0,0);
+        cam.lookAt(0, 0, 0);
         cam.near = 1f;
         cam.far = 300f;
         cam.update();
@@ -91,25 +98,26 @@ public class GameRenderer implements ApplicationListener {
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
         tex1 = new NaiveTexture("tex_1.png");
-        tex2 = new NaiveTexture("tex_2.png");
+        tex2 = new NaiveTexture("glass_ylw.png");
+        tex2a = new NaiveTexture("glass_ylw.png");
         tex3 = new NaiveTexture("tex_3.png");
         atl = new NodeTexAtlas(512, 512);
-        ap1 = new BlockNodeAppearance(tex1, tex1, tex3, tex3, tex1, tex1);
-        ap2 = new BlockNodeAppearance(tex2, tex2, tex2, tex2, tex2, tex2);
-        chk = new OldRenderedChunk(new LoadedMapChunk(gs, 0, 0, 0, nrt), atl);
+        ap1 = new BlockNodeAppearance(RenderBucket.OPAQUE, tex1, tex1, tex3, tex3, tex1, tex1);
+        ap2 = new BlockNodeAppearance(RenderBucket.TRANSPARENT_NO_CULL, tex2, tex2, tex2, tex2, tex2, tex2);
+        chk = new RenderedChunk(new LoadedMapChunk(gs, 0, 0, 0, nrt), atl, shaderManager);
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 16; j++) {
                 for (int k = 0; k < 16; k++) {
                     try {
-                        chk.setNode(i, j, k, HardcodedNodes.AIR);
+                        chk.getDelegate().setNode(i, j, k, HardcodedNodes.AIR);
                     } catch (VxlPluginExecutionException e) {
                         e.printStackTrace();
                     }
                 }
             }
         }
-        chk.clearQueue();
-        chk.onlyPage.rebuild();
+        chk.getDelegate().clearQueue();
+        chk.rebuildAll();
         nd1 = new BaseMapNode("test:n1", ap1);
         nd2 = new BaseMapNode("test:n2", ap2);
         atl.packBatch(tex1, tex2, tex3);
@@ -161,7 +169,7 @@ public class GameRenderer implements ApplicationListener {
                 for (int k = 0; k < 16; k++) {
                     try {
                         //if((i+j+k)%2==0)
-                            chk.setNode(i, j, k, (i>=4 && i < 12)?nd1:nd1);
+                        chk.getDelegate().setNode(i, j, k, (i >= 4 && i < 12) ? nd2 : nd1);
                     } catch (VxlPluginExecutionException e) {
                         e.printStackTrace();
                     }
@@ -172,8 +180,8 @@ public class GameRenderer implements ApplicationListener {
             for (int j = 0; j < 16; j++) {
                 for (int k = 0; k < 16; k++) {
                     try {
-                        if((i/2+j/2+k/2)%2==0)
-                            chk.setNode(i, j, k, HardcodedNodes.AIR);
+                        if ((i / 2 + j / 2 + k / 2) % 2 == 0)
+                            chk.getDelegate().setNode(i, j, k, HardcodedNodes.AIR);
                     } catch (VxlPluginExecutionException e) {
                         e.printStackTrace();
                     }
@@ -192,20 +200,13 @@ public class GameRenderer implements ApplicationListener {
 //                }
 //            }
 //        }
-        lam.addChunk(chk);
-        final String vertexShader = Gdx.files.internal("rigid_vtx.glsl").readString();
-        final String fragmentShader = Gdx.files.internal("rigid_frag.glsl").readString();
-        shader = new RigidBlockNodeShader();
-        shader.init();
+        //lam.addChunk(chk);
         sb = new SpriteBatch();
         mb = new ModelBatch((camera, renderables) -> {
             // no op
         });
         rc.begin();
-
-        shader.begin(cam, rc);
-
-        shader.bindTexture(atl.getGdxAtlas().getTextures().first());
+        shaderManager.setAllContexts(rc);
         Gdx.gl.glEnable(GL13.GL_MULTISAMPLE);
 
     }
@@ -222,81 +223,61 @@ public class GameRenderer implements ApplicationListener {
     int skip = 0;
     boolean usingShader = true;
     boolean lastIncPressed = false;
+    double time = 0;
     @Override
     public void render() {
         camController.update();
 
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         if (frames == 30) {
-            Gdx.graphics.setTitle((usingShader?"OWN":"GDX")+" | QUADS: "+chk.onlyPage.countQuads()+" | FPS: " + Gdx.graphics.getFramesPerSecond());
+            Gdx.graphics.setTitle((usingShader ? "OWN" : "GDX") + " | QUADS: " + chk.getQuadCount() + " | FPS: " + Gdx.graphics.getFramesPerSecond());
             frames = 0;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.F)){
-            usingShader = true;
-            rc.begin();
 
-            shader.begin(cam, rc);
-
-            shader.bindTexture(atl.getGdxAtlas().getTextures().first());
-        }
-        if(Gdx.input.isKeyPressed(Input.Keys.G)){
-            usingShader = false;
-        }
         frames++;
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClearDepthf(1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         //Gdx.gl.glDisable(Gdx.gl20.GL_BLEND);
         //Gdx.gl.glBlendFunc(Gdx.gl.GL_SRC_ALPHA, Gdx.gl.GL_ONE_MINUS_SRC_ALPHA);
-        Gdx.gl.glCullFace(Gdx.input.isKeyPressed(Input.Keys.B)?GL20.GL_NONE:GL20.GL_BACK);
+        Gdx.gl.glCullFace(Gdx.input.isKeyPressed(Input.Keys.B) ? GL20.GL_NONE : GL20.GL_BACK);
         boolean curSpacePressed = Gdx.input.isKeyPressed(Input.Keys.SPACE);
         boolean curIncPressed = Gdx.input.isKeyPressed(Input.Keys.SLASH);
-        if ((curSpacePressed & !lastSpacePressed)||Gdx.input.isKeyPressed(Input.Keys.C)) {
+        if ((curSpacePressed & !lastSpacePressed) || Gdx.input.isKeyPressed(Input.Keys.C)) {
             try {
                 chk.handleOneQueueItem();
-            } catch (VxlPluginExecutionException e) {
-                logger.fatal("Failed to handle a chunk delta. If this happens in production we should rebuild the entire chunk in this case.", e);
+            } catch (Throwable e) {
+                logger.fatal("Failed to handle a chunk delta. If this happens in production we should update the entire chunk in this case.", e);
             }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.A)){
-            while(chk.getQueueBacklog()>0){
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            while (chk.getDelegate().getQueueBacklog() > 0) {
                 try {
                     chk.handleOneQueueItem();
-                } catch (VxlPluginExecutionException e) {
-                    logger.fatal("Failed to handle a chunk delta. If this happens in production we should rebuild the entire chunk in this case.", e);
+                } catch (Throwable e) {
+                    logger.fatal("Failed to handle a chunk delta. If this happens in production we should update the entire chunk in this case.", e);
                 }
             }
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.W)){
-            shader.setWireframe(true);
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            shaderManager.setWireframe(true);
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.Q)){
-            shader.setWireframe(false);
+        if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+            shaderManager.setWireframe(false);
         }
-        if(curIncPressed && !lastIncPressed) skip = (skip+1)%48;
+        if (curIncPressed && !lastIncPressed) skip = (skip + 1) % 48;
         //mb.begin(cam);
         cam.update();
-       //shaderProgram.setUniformMatrix("u_projViewTrans", cam.combined);
+        //shaderProgram.setUniformMatrix("u_projViewTrans", cam.combined);
         //atl.getGdxAtlas().getTextures().first().bind(0);
         //shaderProgram.setUniformi("u_texture", 0);
         int skipped = 0;
-        if(usingShader) {
+        time += Gdx.graphics.getRawDeltaTime();
+        chk.update(cam);
+        shaderManager.updateAllShaders(cam, time);
 
-            shader.update(cam, 0.0);
-            renderables.clear();
-            chk.onlyPage.getGdxModelInstance().getRenderables(renderables, renderablePool);
-            //renderables.reverse();
-            for (Renderable r : renderables) {
-                r.environment = environment;
-                r.worldTransform.idt();
-                if(skipped>=skip) shader.render(r);
-                skipped++;
-            }
-        } else {
-            mb.begin(cam);
-            mb.render(chk.onlyPage.getGdxModelInstance());
-            mb.end();
-        }
+        chk.render();
+
 
         //mb.render(chk.onlyPage.getGdxModelInstance());
         //mb.end();
@@ -317,12 +298,11 @@ public class GameRenderer implements ApplicationListener {
     @Override
     public void dispose() {
 
-        shader.end();
+        shaderManager.disposeAllShaders();
         rc.end();
         tex1.dispose();
         tex2.dispose();
         wrp.dispose();
-        shader.dispose();
 
     }
 }
